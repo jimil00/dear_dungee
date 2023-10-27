659ece6e71a3adbee91a9c2db50be67504828b1b
jimil00 <104720372+jimil00@users.noreply.github.com>
2023-10-27 16:00:52 +0900

    커밋

--- src/main/java/com/dev/dear_dungee/DearDungeeApplication.java
+++ src/main/java/com/dev/dear_dungee/DearDungeeApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import javax.sql.DataSource;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class DearDungeeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DearDungeeApplication.class, args);
+
+		System.out.println("진짜 시작이다!");
 	}
 
 }

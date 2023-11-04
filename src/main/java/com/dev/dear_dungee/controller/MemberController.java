f640f1cd4e4d589e91ce06d5394c3a3d4b4cb9b9
jimil00 <104720372+jimil00@users.noreply.github.com>
2023-11-04 21:28:25 +0900

    thymeleaf 연동 테스트

--- src/main/java/com/dev/dear_dungee/controller/MemberController.java
+++ src/main/java/com/dev/dear_dungee/controller/MemberController.java
@@ -1,8 +1,10 @@
 package com.dev.dear_dungee.controller;
 
+import com.dev.dear_dungee.dto.MemberDTO;
 import com.dev.dear_dungee.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +13,18 @@ public class MemberController {
 
     @Autowired
     private MemberService service;
+
+    @RequestMapping("toSignup")
+    public String toSignup(Model model) {
+
+        String id = "def";
+        MemberDTO dto = service.selectMemberById(id);
+        model.addAttribute("dto", dto);
+
+        System.out.println(dto.getName());
+
+        return "member/signup";
+    }
 
 
 }

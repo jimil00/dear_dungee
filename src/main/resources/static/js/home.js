b73b443290e8e40c8d6b5d908f0ff8e8acb1de56
dltndk9 <dltndk9@gmail.com>
2024-02-07 16:36:34 +0900

    1. search 페이지 레이아웃 정리중 2. search.js /home.js 파일 분리 3. pagehelper 그레들 추가 4. home.html 충돌 해결

---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,11 @@
+$("#sbtn").click(function() {
+     $("#search_word").slideToggle(100);
+     $("#swbtn").slideToggle(100);
+
+        $("#search_word").css('display','block');
+        $("#swbtn").css('display','block');
+
+        $("#search_word").css('display','none');
+        $("#swbtn").css('display','none');
+
+      });

ef7c40df322f2b10fd67731e35a69a6a503cfb40
dltndk9 <dltndk9@gmail.com>
2024-04-08 18:03:43 +0900

    1. 검색 sql문 수정 - 검색어/지역/필요물품로 검색 가능
    2. 검색 페이지 레이아웃 변경
    - 검색 조건 radio 버튼으로 선택
    - 검색 결과 보호소 이름과 주소 정렬
    - 검색 결과 개수 출력
    
    3. SearchDTO 삭제 후 SearchFilterDTO 생성
    
    4. 검색 컨트롤러, DAO 및 service 로직 파라미터 수정
    -Search_word에서 SearchFilterDTO로 변경

--- src/main/java/com/dev/dear_dungee/controller/SearchController.java
+++ src/main/java/com/dev/dear_dungee/controller/SearchController.java
@@ -1,12 +1,18 @@
 package com.dev.dear_dungee.controller;
 
+import com.dev.dear_dungee.dto.SearchFilterDTO;
 import com.dev.dear_dungee.dto.ShelterDTO;
 import com.dev.dear_dungee.service.ShelterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/search/")
@@ -15,29 +21,20 @@ public class SearchController {
     @Autowired
     private ShelterService shelservice;
 
-    //검색 페이지로 이동 //검색 조건 추가
+
+    //검색어 입력 및 검색조건 선택 후 검색 페이지로 이동
     @RequestMapping("ToSearch")
-    public String ToSearch(String search_word, String keyword ,Model model) {
+    public String ToSearch(SearchFilterDTO search_word, Model model) {
 
         model.addAttribute("search_word", search_word);
-
-        model.addAttribute("keyword", keyword);
 
         //보호소 리스트 출력
        List<ShelterDTO> slist = shelservice.selectShelterListBySw(search_word);
 
        model.addAttribute("slist", slist);
 
-
         return "search/search";
     }
-
-    //페이징 처리
-    //public PageInfo<>
-
-
-    //검색 필터링(지역, 필요물품)
-    //검색어랑 검색조건이 같이 넘어가야 함 / 다중검색조건은 여러 개의 값이 넘어가야 함 -> list /
 
 
 }
--- src/main/java/com/dev/dear_dungee/dao/ShelterDAO.java
+++ src/main/java/com/dev/dear_dungee/dao/ShelterDAO.java
@@ -1,5 +1,6 @@
 package com.dev.dear_dungee.dao;
 
+import com.dev.dear_dungee.dto.SearchFilterDTO;
 import com.dev.dear_dungee.dto.ShelterDTO;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +15,8 @@ public class ShelterDAO {
     private SqlSession db;
 
     //검색어로 select
-    public List<ShelterDTO> selectShelterListBySw(String search_word){
+    public List<ShelterDTO> selectShelterListBySw(SearchFilterDTO search_word){
         return db.selectList("Shelter.selectShelterListBySw",search_word);
-    }
-
-    //지역별 select
-    public List<ShelterDTO> selectAllByAddress(String shelter_address){
-        return db.selectList("Shelter.selectAllbyAddress",shelter_address);
-    }
-
-    //필요물품별 select
-    public List<ShelterDTO> selectAllByItem(String item_name){
-        return db.selectList("Shelter.selectAllbyItem",item_name);
     }
 
 }
